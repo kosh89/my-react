57814d757efd3ac8a8441a203dbc3cf78e7d702d
Sergey Vasilyev <kosh.goo@gmail.com>
2020-09-25 15:37:14 +0300

    edit_text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,72 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
-import Login from './Login/Login';
-import Contacts from './Contacts/Contacts';
+const App = () => {
+  const [pValue, setPValue] = useState('some text');
+  const [iValue, setIValue] = useState('');
+  const [isDisabled, setIsDisabled] = useState(true);
 
-const App = () => (
-  <div>
-    {/* <Login /> */}
-    <Contacts />
-  </div>
-);
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    setPValue(iValue);
+    setIsDisabled(true);
+    setIValue('');
+  }
+
+  const onEditButtonClick = () => {
+    setIValue(pValue);
+    setIsDisabled(false);
+  }
+
+  const onRemoveButtonClick = (e) => {
+    e.target.closest('.contact').remove();
+  }
+
+  const onInputChange = (e) => {
+    setIValue(e.target.value);
+  }
+
+  const onOverlayClick = (e) => {
+    if (e.target.classList.contains('overlay')) {
+      setIsDisabled(true);
+      setIValue('');
+    }
+  }
+
+  return (
+    <div className='app'>
+      <div className='contact'>
+        <p value={pValue}>
+          {pValue}
+        </p>
+        <button className='button'
+          onClick={onEditButtonClick}
+          disabled={!isDisabled}>
+          edit text
+        </button>
+        <button className='button button--remove'
+          onClick={onRemoveButtonClick}>
+          remove
+        </button>
+      </div>
+
+      <div className={`modal-edit ${isDisabled ? `` : `show`}`}>
+        <div className='overlay'
+          onClick={onOverlayClick}>
+          <form onSubmit={onFormSubmit}>
+            <input className='input-edit'
+              onChange={onInputChange}
+              value={iValue}
+              type='text' />
+            <button className='button'
+              type='submit'>
+              save
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+};
 
 export default App;
